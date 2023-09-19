c38012ac564a0236d12cac5f8e8529239ee859b7
rash032 <121014393+rash032@users.noreply.github.com>
2023-09-19 18:52:49 +0530

    Add files via upload

---
+++ header_match.go
@@ -0,0 +1,189 @@
+// Package checkheadersplugin plugin.
+package checkheadersplugin
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+//SingleHeader contains a single header keypair
+type SingleHeader struct {
+	Name      string   `json:"name,omitempty"`
+	Values    []string `json:"values,omitempty"`
+	MatchType string   `json:"matchtype,omitempty"`
+	Required  *bool    `json:"required,omitempty"`
+	Contains  *bool    `json:"contains,omitempty"`
+	URLDecode *bool    `json:"urldecode,omitempty"`
+	Debug     *bool    `json:"debug,omitempty"`
+}
+
+// Config the plugin configuration.
+type Config struct {
+	Headers []SingleHeader
+}
+
+// HeaderMatch demonstrates a HeaderMatch plugin.
+type HeaderMatch struct {
+	next    http.Handler
+	headers []SingleHeader
+	name    string
+}
+
+// MatchType defines an enum which can be used to specify the match type for the 'contains' config.
+type MatchType string
+
+const (
+	//MatchAll requires all values to be matched
+	MatchAll MatchType = "all"
+	//MatchOne requires only one value to be matched
+	MatchOne MatchType = "one"
+)
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{
+		Headers: []SingleHeader{},
+	}
+}
+
+// New created a new HeaderMatch plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.Headers) == 0 {
+		return nil, fmt.Errorf("configuration incorrect, missing headers")
+	}
+
+	for _, vHeader := range config.Headers {
+		if strings.TrimSpace(vHeader.Name) == "" {
+			return nil, fmt.Errorf("configuration incorrect, missing header name")
+		}
+		if len(vHeader.Values) == 0 {
+			return nil, fmt.Errorf("configuration incorrect, missing header values")
+		} else {
+			for _, value := range vHeader.Values {
+				if strings.TrimSpace(value) == "" {
+					return nil, fmt.Errorf("configuration incorrect, empty value found")
+				}
+			}
+		}
+		if !vHeader.IsContains() && vHeader.MatchType == string(MatchAll) {
+			return nil, fmt.Errorf("configuration incorrect for header %v %s", vHeader.Name, ", matchall can only be used in combination with 'contains'")
+		}
+		if strings.TrimSpace(vHeader.MatchType) == "" {
+			return nil, fmt.Errorf("configuration incorrect, missing match type configuration for header %v", vHeader.Name)
+		}
+	}
+
+	return &HeaderMatch{
+		headers: config.Headers,
+		next:    next,
+		name:    name,
+	}, nil
+}
+
+func (a *HeaderMatch) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	headersValid := true
+
+	for _, vHeader := range a.headers {
+
+		reqHeaderVal := req.Header.Get(vHeader.Name)
+
+		if vHeader.IsURLDecode() {
+			reqHeaderVal, _ = url.QueryUnescape(reqHeaderVal)
+		}
+
+		if vHeader.IsContains() && reqHeaderVal != "" {
+			headersValid = checkContains(&reqHeaderVal, &vHeader)
+		} else {
+			headersValid = checkRequired(&reqHeaderVal, &vHeader)
+		}
+
+		if vHeader.IsDebug() {
+			fmt.Println("checkheaders (debug):\n\tHeaders valid:", headersValid, "\n\tRequest headers:", reqHeaderVal, "\n\tConfigured headers:", vHeader.Values)
+		}
+
+		if !headersValid {
+			break
+		}
+	}
+
+	if headersValid {
+		a.next.ServeHTTP(rw, req)
+	} else {
+		http.Error(rw, "Not allowed", http.StatusForbidden)
+	}
+}
+
+func checkContains(requestValue *string, vHeader *SingleHeader) bool {
+	matchCount := 0
+	for _, value := range vHeader.Values {
+		if strings.Contains(*requestValue, value) {
+			matchCount++
+		}
+	}
+
+	if matchCount == 0 {
+		return false
+	} else if vHeader.MatchType == string(MatchAll) && matchCount != len(vHeader.Values) {
+		return false
+	}
+
+	return true
+}
+
+func checkRequired(requestValue *string, vHeader *SingleHeader) bool {
+	matchCount := 0
+	for _, value := range vHeader.Values {
+		if *requestValue == value {
+			matchCount++
+		}
+
+		if !vHeader.IsRequired() && *requestValue == "" {
+			matchCount++
+		}
+	}
+
+	if matchCount == 0 {
+		return false
+	}
+
+	return true
+}
+
+//IsURLDecode checks whether a header value should be url decoded first before testing it
+func (s *SingleHeader) IsURLDecode() bool {
+	if s.URLDecode == nil || *s.URLDecode == false {
+		return false
+	}
+
+	return true
+}
+
+//IsDebug checks whether a header value should print debug information in the log
+func (s *SingleHeader) IsDebug() bool {
+	if s.Debug == nil || *s.Debug == false {
+		return false
+	}
+
+	return true
+}
+
+//IsContains checks whether a header value should contain the configured value
+func (s *SingleHeader) IsContains() bool {
+	if s.Contains == nil || *s.Contains == false {
+		return false
+	}
+
+	return true
+}
+
+//IsRequired checks whether a header is mandatory in the request, defaults to 'true'
+func (s *SingleHeader) IsRequired() bool {
+	if s.Required == nil || *s.Required != false {
+		return true
+	}
+
+	return false
+}
---
+++ header_match_test.go
@@ -0,0 +1,158 @@
+package checkheadersplugin_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	checkheaders "github.com/dkijkuit/checkheadersplugin"
+)
+
+var required = true
+var not_required = false
+var contains = true
+var urlDecode = true
+
+var testcert = `
+Subject%3D%22C%3DNL%2CST%3DST-TEST%2CL%3DCity%2CO%3DOrganization%2CCN%3Dcommon-name%22%3BIssuer%3D%22DC%3Dnl%2CDC%3Ddomainpart1%2CDC%3Ddomainpart2%2CCN%3DSomeKindOfCa%22%3BNB%3D%221589744159%22%3BNA%3D%221765837153%22%3BSAN%3D%22somkindofdomain.domain.thing.test%22
+`
+
+func TestHeadersMatch(t *testing.T) {
+	requestHeaders := map[string]string{
+		"test1":                            "testvalue1",
+		"test2":                            "testvalue2",
+		"test3":                            "testvalue3",
+		"test4":                            "value4",
+		"X-Forwarded-Tls-Client-Cert-Info": testcert,
+		"testMultipleContainsValues":       "value5_or_value1_or_value_2_or_value_3",
+	}
+
+	executeTest(t, requestHeaders, http.StatusOK)
+}
+
+func TestHeadersOneMatch(t *testing.T) {
+	requestHeaders := map[string]string{
+		"test1":                            "testvalue1",
+		"test2":                            "testvalue2",
+		"test3":                            "testvalue3",
+		"test4":                            "value4",
+		"X-Forwarded-Tls-Client-Cert-Info": testcert,
+		"testMultipleContainsValues":       "test_or_value2",
+	}
+
+	executeTest(t, requestHeaders, http.StatusOK)
+}
+
+func TestHeadersNotMatch(t *testing.T) {
+	requestHeaders := map[string]string{
+		"test1":                            "wrongvalue1",
+		"test2":                            "wrongvalue2",
+		"test3":                            "wrongvalue3",
+		"test4":                            "correctvalue4",
+		"X-Forwarded-Tls-Client-Cert-Info": "wrongvalue",
+		"testMultipleContainsValues":       "wrongvalues",
+	}
+
+	executeTest(t, requestHeaders, http.StatusForbidden)
+}
+
+func TestHeadersNotRequired(t *testing.T) {
+	requestHeaders := map[string]string{
+		"test1":                            "testvalue1",
+		"test2":                            "testvalue2",
+		"test4":                            "ue4",
+		"X-Forwarded-Tls-Client-Cert-Info": testcert,
+		"testMultipleContainsValues":       "value5_or_value1_or_value_2_or_value_3",
+	}
+
+	executeTest(t, requestHeaders, http.StatusOK)
+}
+
+func executeTest(t *testing.T, requestHeaders map[string]string, expectedResultCode int) {
+	cfg := checkheaders.CreateConfig()
+	cfg.Headers = []checkheaders.SingleHeader{
+		{
+			Name:      "test1",
+			MatchType: string(checkheaders.MatchOne),
+			Values:    []string{"testvalue1"},
+		},
+		{
+			Name:      "test2",
+			MatchType: string(checkheaders.MatchOne),
+			Values:    []string{"testvalue2"},
+			Required:  &required,
+		},
+		{
+			Name:      "test3",
+			MatchType: string(checkheaders.MatchOne),
+			Values:    []string{"testvalue3"},
+			Required:  &not_required,
+		},
+		{
+			Name:      "test4",
+			MatchType: string(checkheaders.MatchOne),
+			Values:    []string{"ue4"},
+			Required:  &required,
+			Contains:  &contains,
+		},
+		{
+			Name: "X-Forwarded-Tls-Client-Cert-Info",
+			Values: []string{
+				"CN=common-name",
+				"SAN=\"somkindofdomain.domain.thing.test\"",
+			},
+			MatchType: string(checkheaders.MatchAll),
+			Required:  &required,
+			Contains:  &contains,
+			URLDecode: &urlDecode,
+		},
+		{
+			Name: "testMultipleContainsValues",
+			Values: []string{
+				"value1",
+				"or_value2",
+			},
+			MatchType: string(checkheaders.MatchOne),
+			Required:  &required,
+			Contains:  &contains,
+			URLDecode: &urlDecode,
+		},
+		{
+			Name: "testContainsNotRequired",
+			Values: []string{
+				"value_not_important",
+				"value_not_important_2",
+			},
+			MatchType: string(checkheaders.MatchOne),
+			Required:  &not_required,
+			Contains:  &contains,
+			URLDecode: &urlDecode,
+		},
+	}
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := checkheaders.New(ctx, next, cfg, "check-headers-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	for headerName, headerValue := range requestHeaders {
+		req.Header.Add(headerName, headerValue)
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	if recorder.Result().StatusCode != expectedResultCode {
+		t.Errorf("Unexpected response status code: %d, expected: %d for incoming request headers: %s", recorder.Result().StatusCode, expectedResultCode, requestHeaders)
+	}
+}
